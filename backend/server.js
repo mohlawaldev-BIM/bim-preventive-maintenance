57fbe814b5eb563be25c479c2ae3380ef305fad1
Moh Lawal <mohlawal.dev@gmail.com>
2026-04-03 01:50:42 +0100

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,15 @@ const projectRoutes = require('./routes/projects');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'https://bim-preventive-maintenance.vercel.app',
+  ],
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api/assets', assetRoutes);
